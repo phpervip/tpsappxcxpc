61bda84801d8808f2e05c0b7164d8b42a32d2057
zhangyi <2998658517@qq.com>
2019-01-24 16:56:36 +0800

    Update DEFAULT_ACTION Index to index

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -113,7 +113,7 @@ return array(
     
     'DEFAULT_MODULE'        =>  'Home',  // 默认模块
     'DEFAULT_CONTROLLER'    =>  'Index', // 默认控制器名称
-    'DEFAULT_ACTION'        =>  'Index', // 默认操作名称
+    'DEFAULT_ACTION'        =>  'index', // 默认操作名称
 
     'goods_state' => array(
         '0'=>'待审核',
